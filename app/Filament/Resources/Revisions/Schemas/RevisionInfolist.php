1c9c89843ab1ed886e6f8933b67a2cee47ba4401
Benjamin Crozat <benjamincrozat@me.com>
2025-08-31 17:30:20 +0200

    Add copy buttons to revision fields

--- app/Filament/Resources/Revisions/Schemas/RevisionInfolist.php
+++ app/Filament/Resources/Revisions/Schemas/RevisionInfolist.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Resources\Revisions\Schemas;
 
 use App\Models\Revision;
+use Illuminate\Support\Js;
+use Filament\Actions\Action;
 use Filament\Schemas\Schema;
 use Filament\Schemas\Components\Grid;
 use Filament\Infolists\Components\TextEntry;
@@ -16,27 +18,81 @@ class RevisionInfolist
                 Grid::make()
                     ->schema([
                         TextEntry::make('report.post.title')
-                            ->label('Revision for'),
+                            ->label('Revision for')
+                            ->afterContent(fn (TextEntry $component) => Action::make('copy_revision_for')
+                                ->label('Copy')
+                                ->icon('heroicon-m-clipboard')
+                                ->size('xs')
+                                ->extraAttributes(fn () => [
+                                    'x-on:click' => 'window.navigator.clipboard.writeText('
+                                        . Js::from($component->getState())
+                                        . '); $tooltip(\'Copied to clipboard\', { theme: $store.theme })',
+                                ])),
 
                         TextEntry::make('created_at')
                             ->dateTime()
-                            ->label('Creation Date'),
+                            ->label('Creation Date')
+                            ->afterContent(fn (TextEntry $component) => Action::make('copy_creation_date')
+                                ->label('Copy')
+                                ->icon('heroicon-m-clipboard')
+                                ->size('xs')
+                                ->extraAttributes(fn () => [
+                                    'x-on:click' => 'window.navigator.clipboard.writeText('
+                                        . Js::from((string) $component->formatState($component->getState()))
+                                        . '); $tooltip(\'Copied to clipboard\', { theme: $store.theme })',
+                                ])),
 
                         TextEntry::make('updated_at')
                             ->dateTime()
-                            ->label('Last Modification Date'),
+                            ->label('Last Modification Date')
+                            ->afterContent(fn (TextEntry $component) => Action::make('copy_last_modified')
+                                ->label('Copy')
+                                ->icon('heroicon-m-clipboard')
+                                ->size('xs')
+                                ->extraAttributes(fn () => [
+                                    'x-on:click' => 'window.navigator.clipboard.writeText('
+                                        . Js::from((string) $component->formatState($component->getState()))
+                                        . '); $tooltip(\'Copied to clipboard\', { theme: $store.theme })',
+                                ])),
 
                         TextEntry::make('title')
-                            ->state(fn (Revision $record) => $record->data['title']),
+                            ->state(fn (Revision $record) => $record->data['title'])
+                            ->afterContent(fn (TextEntry $component) => Action::make('copy_title')
+                                ->label('Copy')
+                                ->icon('heroicon-m-clipboard')
+                                ->size('xs')
+                                ->extraAttributes(fn () => [
+                                    'x-on:click' => 'window.navigator.clipboard.writeText('
+                                        . Js::from($component->getState())
+                                        . '); $tooltip(\'Copied to clipboard\', { theme: $store.theme })',
+                                ])),
 
                         TextEntry::make('description')
-                            ->state(fn (Revision $record) => $record->data['description']),
+                            ->state(fn (Revision $record) => $record->data['description'])
+                            ->afterContent(fn (TextEntry $component) => Action::make('copy_description')
+                                ->label('Copy')
+                                ->icon('heroicon-m-clipboard')
+                                ->size('xs')
+                                ->extraAttributes(fn () => [
+                                    'x-on:click' => 'window.navigator.clipboard.writeText('
+                                        . Js::from($component->getState())
+                                        . '); $tooltip(\'Copied to clipboard\', { theme: $store.theme })',
+                                ])),
                     ])
                     ->columnSpanFull(),
 
                 TextEntry::make('content')
                     ->state(fn (Revision $record) => $record->data['content'])
                     ->markdown()
+                    ->afterContent(fn (TextEntry $component) => Action::make('copy_content')
+                        ->label('Copy')
+                        ->icon('heroicon-m-clipboard')
+                        ->size('xs')
+                        ->extraAttributes(fn () => [
+                            'x-on:click' => 'window.navigator.clipboard.writeText('
+                                . Js::from($component->getState())
+                                . '); $tooltip(\'Copied to clipboard\', { theme: $store.theme })',
+                        ]))
                     ->columnSpan(2),
             ])
             ->columns(3);
