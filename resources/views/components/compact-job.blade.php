7a9738183edeccf682615b643441feca470d6f68
Benjamin Crozat <hello@benjamincrozat.com>
2025-11-19 23:25:12 +0100

    WIP

--- resources/views/components/compact-job.blade.php
+++ resources/views/components/compact-job.blade.php
@@ -5,30 +5,32 @@
         <x-heroicon-o-building-office class="text-gray-500 size-6" />
     </div>
 
-    <div>
-        <a
-            wire:navigate
-            href="{{ route('jobs.show', $job->slug) }}"
-            class="font-bold transition-colors hover:text-blue-600"
-            data-pirsch-event="Clicked job"
-            data-pirsch-meta-title="{{ $job->title }}"
-        >
-            {{ $job->title }}
-        </a>
-
-        <div class="flex items-center gap-3 mt-1">
-            <p>
+    <div class="grow">
+        <div class="flex justify-between">
+            <p class="text-gray-500">
                 {{ $job->company->name }}
             </p>
 
-            @if ($job->min_salary && $job->max_salary)
-                <div class="text-xs opacity-50">/</div>
-
-                <p class="flex items-center gap-2">
-                    <x-heroicon-o-banknotes class="size-4 opacity-75" />
-                    {{ Number::currency($job->min_salary, $job->currency ?? 'USD') }}—{{ Number::currency($job->max_salary, $job->currency ?? 'USD') }}
-                </p>
-            @endif
+            <p class="text-gray-500">
+                {{ $job->created_at->diffForHumans() }}
+            </p>
         </div>
+
+        <p class="mt-1">
+            <a
+                wire:navigate
+                href="{{ route('jobs.show', $job->slug) }}"
+                class="font-bold text-black transition-colors hover:text-blue-600"
+                data-pirsch-event="Clicked job"
+                data-pirsch-meta-title="{{ $job->title }}"
+            >
+                {{ $job->title }}
+            </a>
+        </p>
+
+        <p class="flex items-center gap-2 mt-1 font-medium">
+            <x-heroicon-o-banknotes class="size-4 opacity-75" />
+            {{ Number::currency($job->min_salary, $job->currency ?? 'USD') }}—{{ Number::currency($job->max_salary, $job->currency ?? 'USD') }}
+        </p>
     </div>
 </div>
