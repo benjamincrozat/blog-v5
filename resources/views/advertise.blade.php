daed4da9f5cce22370088fd45ce5377c0e94f097
Benjamin Crozat <hello@benjamincrozat.com>
2025-11-22 11:34:35 +0100

    WIP

--- resources/views/advertise.blade.php
+++ resources/views/advertise.blade.php
@@ -3,12 +3,6 @@
     :hide-ad="true"
 >
     <div class="container text-center md:text-xl xl:max-w-(--breakpoint-lg)">
-        <img
-            loading="lazy"
-            src="{{ Vite::asset('resources/img/icons/megaphone.png') }}"
-            class="mx-auto mb-8 h-24 md:h-28 lg:h-32"
-        />
-
         <h1 class="text-3xl font-medium tracking-tight text-black lg:text-4xl xl:text-7xl">
             Advertise to <span class="text-blue-600">{{ Number::format($visitors) }}</span>&nbsp;developers
         </h1>
@@ -17,26 +11,190 @@
             This is the right place to show off your product.
         </p>
 
-        <x-btn
-            primary
-            size="md"
-            href="#products"
-            class="table mx-auto mt-8 lg:mt-12"
-        >
-            Learn more
-        </x-btn>
+        <div class="flex flex-wrap md:flex-nowrap text-center md:text-left gap-4 justify-center">
+            <x-btn
+                size="md"
+                href="https://benjamincrozat.pirsch.io/?domain=benjamincrozat.com&interval=30d&scale=day"
+                target="_blank"
+                class="mt-8 lg:mt-12"
+            >
+                My analytics dashboard
+            </x-btn>
+
+            <x-btn
+                primary
+                size="md"
+                href="#products"
+                class="mt-8 lg:mt-12"
+            >
+                Check offers
+            </x-btn>
+        </div>
     </div>
 
     <x-section
         title="Trusted by"
         class="mt-24 text-center"
     >
-        <div class="flex flex-wrap gap-y-4 gap-x-8 justify-center items-center px-4 md:gap-x-12 lg:gap-x-16">
-            <x-icon-coderabbit class="flex-none h-6 sm:h-7" />
-            <x-icon-kinsta class="flex-none -translate-y-px h-[1.15rem] sm:h-6" />
+        <div class="flex flex-wrap md:flex-nowrap text-center md:text-left flex-wrap gap-y-4 gap-x-8 justify-center items-center px-4 md:gap-x-12 lg:gap-x-16">
+            <x-icon-coderabbit class="flex-none translate-y-[.5px] sm:translate-y-0 h-[1.35rem] sm:h-[1.65rem]" />
+            <x-icon-kinsta class="flex-none -translate-y-px sm:translate-y-0 h-[1.15rem] sm:h-[1.35rem]" />
             <div class="text-2xl font-bold text-red-600 sm:text-3xl">larajobs</div>
-            <x-icon-meilisearch class="flex-none h-6 translate-y-px sm:h-7" />
+            <x-icon-meilisearch class="flex-none h-6 translate-y-px sm:h-8" />
             <x-icon-sevalla class="flex-none h-9 sm:h-10" />
+        </div>
+    </x-section>
+
+    <a name="products"></a>
+
+    <x-section
+        id="top-banner"
+        class="mt-24 xl:max-w-(--breakpoint-lg)"
+    >
+        <x-heading>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" class="size-16 mx-auto mb-2"><path d="M40 56h176v40H40z" opacity=".2"/><path d="M216 40H40c-8.836556 0-16 7.163444-16 16v144c0 8.836556 7.163444 16 16 16h176c8.836556 0 16-7.163444 16-16V56c0-8.836556-7.163444-16-16-16Zm0 16v40H40V56h176Zm0 144H40v-88h176v88Z"/></svg>
+            Stay on top of every page
+        </x-heading>
+
+        <div class="flex flex-wrap md:flex-nowrap text-center md:text-left items-start mt-8 justify-between gap-8 md:gap-16">
+            <div class="md:w-1/2 w-full">
+                <p class="font-medium">What you get:</p>
+                
+                <ul class="grid gap-2 mt-2 text-left">
+                    <li class="flex gap-2 items-start">
+                        <x-heroicon-o-check class="flex-none text-green-600 translate-y-1 size-4" />
+                        Featured on top of every page.
+                    </li>
+        
+                    <li class="flex gap-2 items-start">
+                        <x-heroicon-o-check class="flex-none text-green-600 translate-y-1 size-4" />
+                        Access to {{ Number::format($visitors) }} monthly developers.
+                    </li>
+        
+                    <li class="flex gap-2 items-start">
+                        <x-heroicon-o-check class="flex-none text-green-600 translate-y-1 size-4" />
+                        A backlink on a DR 51 domain.
+                    </li>
+                </ul>
+            </div>
+
+            <div class="md:w-1/2 w-full">
+                <p class="text-7xl font-medium">600€</p>
+                <p class="text-sm text-gray-500">one-time payment</p>
+            
+                <div class="flex flex-wrap md:flex-nowrap text-center md:text-left items-center gap-4 mt-4 md:mt-8 justify-center md:justify-start">
+                    <x-btn href="https://benjamincrozat.com/php-85" target="_blank">
+                        Live example
+                    </x-btn>
+                    
+                    <x-btn primary disabled>
+                        Sold out!
+                    </x-btn>
+                </div>
+    
+                <p class="mt-4 md:mt-8 text-balance">Once done, <a href="mailto:hello@benjamincrozat.com" class="font-medium underline">email me</a> with the necessary information and you will be live within 24 hours.</p>
+            </div>
+        </div>
+    </x-section>
+
+    <x-section
+        id="top-banner"
+        class="mt-24 xl:max-w-(--breakpoint-lg)"
+    >
+        <x-heading>
+            <x-phosphor-sidebar-duotone class="size-16 mx-auto mb-2 -scale-x-100" />
+            Be visible in the sidebar
+        </x-heading>
+
+        <div class="flex flex-wrap md:flex-nowrap text-center md:text-left items-start mt-8 justify-between gap-8 md:gap-16">
+            <div class="md:w-1/2 w-full">
+                <p class="font-medium">What you get:</p>
+                
+                <ul class="grid gap-2 mt-2 text-left">
+                    <li class="flex gap-2 items-start">
+                        <x-heroicon-o-check class="flex-none text-green-600 translate-y-1 size-4" />
+                        Visibility in the sidebar on every article.
+                    </li>
+        
+                    <li class="flex gap-2 items-start">
+                        <x-heroicon-o-check class="flex-none text-green-600 translate-y-1 size-4" />
+                        Access to {{ Number::format($visitors) }} monthly developers.
+                    </li>
+        
+                    <li class="flex gap-2 items-start">
+                        <x-heroicon-o-check class="flex-none text-green-600 translate-y-1 size-4" />
+                        A backlink on a DR 51 domain.
+                    </li>
+                </ul>
+            </div>
+
+            <div class="md:w-1/2 w-full">
+                <p class="text-7xl font-medium">400€</p>
+                <p class="text-sm text-gray-500">one-time payment</p>
+            
+                <div class="flex flex-wrap md:flex-nowrap text-center md:text-left items-center gap-4 mt-4 md:mt-8 justify-center md:justify-start">
+                    <x-btn href="https://benjamincrozat.com/php-85" target="_blank">
+                        Live example
+                    </x-btn>
+                    
+                    <x-btn primary disabled>
+                        Sold out!
+                    </x-btn>
+                </div>
+    
+                <p class="mt-4 md:mt-8 text-balance">Once done, <a href="mailto:hello@benjamincrozat.com" class="font-medium underline">email me</a> with the necessary information and you will be live within 24 hours.</p>
+            </div>
+        </div>
+    </x-section>
+
+    <x-section
+        title="Write a sponsored article"
+        id="sponsored-article"
+        class="mt-24 xl:max-w-(--breakpoint-lg)"
+    >
+        <div class="flex flex-wrap md:flex-nowrap text-center md:text-left items-start justify-between gap-8 md:gap-16">
+            <div class="md:w-1/2 w-full">
+                <p class="font-medium">What you get:</p>
+                
+                <ul class="grid gap-2 mt-2 text-left">
+                    <li class="flex gap-2 items-start">
+                        <x-heroicon-o-check class="flex-none text-green-600 translate-y-1 size-4" />
+                        Featured on top of every article from Monday to Sunday.
+                    </li>
+        
+                    <li class="flex gap-2 items-start">
+                        <x-heroicon-o-check class="flex-none text-green-600 translate-y-1 size-4" />
+                        Access to {{ Number::format($visitors) }} monthly developers.
+                    </li>
+        
+                    <li class="flex gap-2 items-start">
+                        <x-heroicon-o-check class="flex-none text-green-600 translate-y-1 size-4" />
+                        A backlink on a DR 51 domain.
+                    </li>
+        
+                    <li class="flex gap-2 items-start">
+                        <x-heroicon-o-check class="flex-none text-green-600 translate-y-1 size-4" />
+                        <span>A secured position on the blog, <strong class="font-medium">forever</strong>.</span>
+                    </li>
+                </ul>
+            </div>
+
+            <div  class="w-1/2">
+                <p class="text-7xl font-medium">500€</p>
+                <p class="text-sm text-gray-500">one-time payment</p>
+            
+                <div class="flex flex-wrap md:flex-nowrap text-center md:text-left items-center gap-4 mt-4 md:mt-8 justify-center md:justify-start">
+                    <x-btn href="https://benjamincrozat.com/deploy-php-laravel-apps-sevalla" target="_blank">
+                        Live example
+                    </x-btn>
+                    
+                    <x-btn href="{{ route('checkout.start', 'sponsored_article') }}" primary>
+                        Get started
+                    </x-btn>
+                </div>
+    
+                <p class="mt-4 md:mt-8 text-balance">Once done, <a href="mailto:hello@benjamincrozat.com" class="font-medium underline">email me</a> with your article. Here are the <a wire:navigate href="{{ route('advertise.guidelines') }}" class="font-medium underline">guidelines</a>. You will be live within 24 hours.</p>
+            </div>
         </div>
     </x-section>
 
@@ -69,47 +227,9 @@
                 <div class="md:text-xl lg:text-xl">on desktop</div>
             </div>
         </div>
-    </x-section>
 
-    <x-section
-        title="Write a sponsored article"
-        id="products"
-        class="mt-24 lg:max-w-(--breakpoint-md)"
-    >
-        <div class="p-8 text-center bg-white rounded-xl ring-1 shadow-xl shadow-black/5 ring-black/10">
-            <p class="text-7xl font-medium">500€</p>
-
-            <p class="text-sm text-gray-500">one-time payment</p>
-
-            <p class="mt-8 font-medium">What you get:</p>
-
-            <ul class="grid gap-2 place-content-center mt-2 text-left">
-                <li class="flex gap-2 items-start">
-                    <x-heroicon-o-check class="flex-none text-green-600 translate-y-1 size-4" />
-                    Featured on top of every article from Monday to Sunday
-                </li>
-
-                <li class="flex gap-2 items-start">
-                    <x-heroicon-o-check class="flex-none text-green-600 translate-y-1 size-4" />
-                    Access to {{ Number::format($visitors) }} monthly developers
-                </li>
-
-                <li class="flex gap-2 items-start">
-                    <x-heroicon-o-check class="flex-none text-green-600 translate-y-1 size-4" />
-                    A backlink on a DR 51 domain
-                </li>
-
-                <li class="flex gap-2 items-start">
-                    <x-heroicon-o-check class="flex-none text-green-600 translate-y-1 size-4" />
-                    <span>A secured position on the blog, <strong class="font-medium">forever</strong></span>
-                </li>
-            </ul>
-
-            <x-btn href="{{ route('checkout.start', 'sponsored_article') }}" primary class="mt-8">
-                Pay and publish
-            </x-btn>
-
-            <p class="mt-8 text-balance">Once done, <a href="mailto:hello@benjamincrozat.com" class="font-medium underline">email me</a> with your article. Here are the <a wire:navigate href="{{ route('advertise.guidelines') }}" class="font-medium underline">guidelines</a>. It will be published as soon as possible today or tomorrow.</p>
-        </div>
+        <x-btn primary href="#products" class="mt-8 table mx-auto">
+            Check offers
+        </x-btn>
     </x-section>
 </x-app>
