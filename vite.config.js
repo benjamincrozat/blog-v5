58657ab58752ad55507015be6b694d4dddf9137c
Benjamin Crozat <hello@benjamincrozat.com>
2026-03-13 21:19:10 +0100

    Updated Vite config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,11 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-
         tailwindcss(),
     ],
+    server: {
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+    },
 })
