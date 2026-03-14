65682612c5c07a5802334f2f7430c9c0de971bfe
Benjamin Crozat <hello@benjamincrozat.com>
2026-03-14 19:45:18 +0100

    Copy update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@ Displays the home view.
         </x-typography.headline>
 
         <x-typography.subheadline class="mt-6 md:mt-10">
-            Stay ahead in web development with practical news, techniques, tools, and a good dose of AI.
+            Stay ahead in web development with a good dose of AI and everything else.
         </x-typography.subheadline>
 
         <div class="flex gap-2 justify-center items-center mt-7 text-center md:mt-11">
