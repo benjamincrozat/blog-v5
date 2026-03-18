7c0203fb54d005ad6a21887dd008ec432d4230a5
Benjamin Crozat <hello@benjamincrozat.com>
2026-03-18 19:54:46 +0100

    Copy fix

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -3,19 +3,15 @@ Displays the categories show view.
 --}}
 
 <x-app
-    :title="$category->name . ' articles, news, takes, and tutorials'"
+    :title="'Articles, news, takes, and tutorials about ' . $category->name"
     :description="'Browse articles, news, takes, and tutorials about ' . $category->name . ' for web developers.'"
 >
-    <article class="container xl:max-w-(--breakpoint-lg)">
+    <article class="container">
         <x-breadcrumbs :items="$breadcrumbs" class="mb-12 md:mb-14" />
 
-        <x-typography.headline>
-            Stay ahead in <span class="text-blue-600">{{ $category->name }}</span>
+        <x-typography.headline class="mx-auto max-w-screen-md">
+            Articles, news, takes, and tutorials about <span class="text-blue-600">{{ $category->name }}</span>
         </x-typography.headline>
-
-        <x-typography.subheadline class="mt-6 md:mt-10">
-            Articles, news, takes, and tutorials for web developers who want to keep up with {{ $category->name }}.
-        </x-typography.subheadline>
 
         <x-posts-grid :$posts class="mt-16 md:mt-24" />
 
