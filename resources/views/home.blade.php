44a6d76e5f968f1cbf0c89a53f670ab01d313319
Benjamin Crozat <hello@benjamincrozat.com>
2026-08-15 14:09:05 +0200

    Move homepage browse links into headers
    - Align each browse link opposite its section title.
    
    - Replace bottom primary buttons with contextual text links.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,34 +13,44 @@ Displays the home view.
         </x-typography.subheadline>
     </div>
 
-    <x-section title="Latest posts" id="latest" class="mt-24 md:mt-32">
+    <x-section id="latest" class="mt-24 md:mt-32">
+        <div class="flex gap-4 justify-between items-baseline mb-8">
+            <x-typography.heading tag="h2" class="text-left!">
+                Latest posts
+            </x-typography.heading>
+
+            <a
+                wire:navigate
+                href="{{ route('posts.index') }}"
+                class="shrink-0 font-medium underline transition-colors hover:text-blue-600 underline-offset-4 decoration-1 decoration-black/30 hover:decoration-blue-600/50"
+            >
+                Browse all articles
+            </a>
+        </div>
+
         @if ($latest->isNotEmpty())
             <x-posts-grid :posts="$latest" />
         @endif
-
-        <x-btn
-            primary
-            wire:navigate
-            href="{{ route('posts.index') }}"
-            class="table mx-auto mt-16"
-        >
-            Browse all articles
-        </x-btn>
     </x-section>
 
-    <x-section title="Latest links" id="links" class="mt-24 md:mt-32">
+    <x-section id="links" class="mt-24 md:mt-32">
+        <div class="flex gap-4 justify-between items-baseline mb-8">
+            <x-typography.heading tag="h2" class="text-left!">
+                Latest links
+            </x-typography.heading>
+
+            <a
+                wire:navigate
+                href="{{ route('links.index') }}"
+                class="shrink-0 font-medium underline transition-colors hover:text-blue-600 underline-offset-4 decoration-1 decoration-black/30 hover:decoration-blue-600/50"
+            >
+                Browse all links
+            </a>
+        </div>
+
         @if ($links->isNotEmpty())
             <x-links-grid :$links :show-images="false" />
         @endif
-
-        <x-btn
-            primary
-            wire:navigate
-            href="{{ route('links.index') }}"
-            class="table mx-auto mt-16"
-        >
-            Browse all links
-        </x-btn>
     </x-section>
 
 </x-app>
